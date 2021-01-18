456ae073853e7fe8bccd79f3950a61b0345e05f6
Olumayokun Fowowe <olumayokun@gmail.com>
2021-01-18 18:41:40 +0100

    completed oauth2

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::namespace('App\Http\Controllers\Api')->group(function(){
+
+    Route::prefix('auth')->group(function(){
+
+        Route::post('login', 'AuthController@login');
+        Route::post('signup', 'AuthController@signup');
+
+    });
+
+    Route::group([
+        'middleware'=>'auth:api'
+    ], function(){
+
+        Route::get('helloworld', 'AuthController@index');
+        Route::post('logout', 'AuthController@logout');
+
+    });
+
 });
